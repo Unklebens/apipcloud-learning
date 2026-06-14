eb8339156513c8005dae380e7cc0f8a9e60895fa
Fahim <contact@fkds.fr>
2026-06-14 16:26:10 +0200

    transfer progress now displayed

--- upload.sh
+++ upload.sh
@@ -2,26 +2,47 @@
 set -euo pipefail
 TOKEN=$(cat tokenfile)
 
-LOCAL_FILE="/mnt/c/Users/Fahim/Downloads/Reolink_Client_User_Manual.pdf"
-LOCAL_FILENAME="Reolink_Client_User_Manual.pdf"
+LOCAL_FILE="/mnt/c/Users/Fahim/Downloads/Samsung_Magician_Installer_Official_9.0.0.910.exe"
+LOCAL_FILENAME=$(basename "$LOCAL_FILE")
 
+PROGRESS_HASH=$(uuidgen) # hash unique pour suivre la progression de l'upload
+TMPFILE=/tmp/$PROGRESS_HASH # fichier temporaire pour stocker la réponse de l'upload
+touch $TMPFILE
 
-#  curl -fsSl -G "https://eapi.pcloud.com/listfolder"\
-#    --data-urlencode "auth=$TOKEN" \
-#      --data-urlencode "folderid=0" \
-
-#  curl -fsSl "https://eapi.pcloud.com/uploadfile"\
-#    -F "auth=$TOKEN" \./up 
-#    -F "folderid=0" \
-#    -F "filename=$LOCAL_FILENAME" \
-#    -F "file=@$LOCAL_FILE"
-
-RESPONSE=$(curl -fsSL \
+curl -fsSL \
   "https://eapi.pcloud.com/uploadfile" \
   -F "auth=$TOKEN" \
   -F "folderid=0" \
+  -F "progresshash=$PROGRESS_HASH" \
   -F "filename=$LOCAL_FILENAME" \
-  -F "file=@$LOCAL_FILE")
+  -F "file=@$LOCAL_FILE" > "$TMPFILE" &
+UPLOAD_PID=$!  # PID du curl en background
+
+echo "transfering $LOCAL_FILENAME : $PROGRESS_HASH"
+sleep 2 # attendre un peu avant de vérifier la progression
+
+while kill -0 $UPLOAD_PID 2>/dev/null; do # tant que le processus d'upload est actif
+  UPLOADPROGRESS=$(curl -fsSL -G "https://eapi.pcloud.com/uploadprogress" \
+    --data-urlencode "auth=$TOKEN" \
+    --data-urlencode "progresshash=$PROGRESS_HASH")
+  
+  UPR=$(echo $UPLOADPROGRESS | jq -r '.result')
+
+  if [ "$UPR" -eq 1900 ]; then
+    echo "Transfer initiating"
+    else
+    TOTAL=$(echo $UPLOADPROGRESS | jq -r '.total')
+    UPLOADED=$(echo $UPLOADPROGRESS | jq -r '.uploaded')
+    PERCENTAGE=$((UPLOADED * 100 / TOTAL))
+    echo "Upload progress: $PERCENTAGE% ($UPLOADED/$TOTAL bytes)"
+  fi
+  sleep 2
+done
+
+wait $UPLOAD_PID  # attend la fin proprement
+
+RESPONSE=$(cat "$TMPFILE")
+rm -f "$TMPFILE"
 
 RESULT=$(echo $RESPONSE | jq '.result')
 
