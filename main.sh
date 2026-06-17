7440116245e3591d70ba1f3b6702d6f972ce1737
Microscotch <github.com@microscotch.net>
2026-06-18 01:57:32 +0200

    Refactor main.sh for improved file upload handling
    En bash preferer [[ ]] pour les tests en lieu et place de [ ]
    Be POSIX: preferrer ${var} instead of $var

--- main.sh
+++ main.sh
@@ -1,51 +1,47 @@
+#!/bin/bash # Pas certain, mais le sheebang doit être la premiere instruction
 # main.sh
-#!/bin/bash
 
 ENV_FILE="../homelab/.secret/pcloud.env" #<-- fichier d'environnement contenant les variables d'environnement nécessaires
-#PCLOUDUSER=
-#PCLOUDPASS=
-[ -f "$ENV_FILE" ] && source "$ENV_FILE" # la condition permet d'utiliser le script manuellement et dans le container qui aura des env
-source functions.sh
+: $[PCLOUDUSER;?variable non definie} # Leve une exception si la variable est absente
+: ${PCLOUDPASS;?variable non definie} # Leve une exception si la variable est absente
+[[ ! -f "${ENV_FILE}" ]] || : ${EXCEPTION:?{$ENV_FILE}: illisible ou absent}
 
-if [ $# -lt 1 ]; then
-    echo "No file provided. Exiting."
-    exit 1
+source "${ENV_FILE}" # la condition permet d'utiliser le script manuellement et dans le container qui aura des env
+source functions.sh || : ${EXCEPTION:?function.sh: illisible ou absent}
 
-else 
-    login
-    get_quota
-    TOTAL=$#
-    COUNT=0
-    SUCCESS_FILES=()
-    FAIL_FILES=()
-    for f in "$@"; do
-        COUNT=$((COUNT + 1))
-        echo "Transferring file $COUNT/$TOTAL"
-        FILESIZE=$(du -b "$f" | cut -f1)
-        if [ $FILESIZE -gt $FREEQUOTA ]; then
-            FILESIZE_MB=$(( FILESIZE / 1024 / 1024 ))
-            echo "File $f is too large to upload ($FILESIZE_MB MB). Skipping."
-            FAIL_FILES+=("$f")
-            continue
-        fi
-        upload "$f"
-        if [ $? -eq 0 ]; then
-            SUCCESS_FILES+=("$f")
-            FREEQUOTA=$((FREEQUOTA - FILESIZE))
-        else
-            FAIL_FILES+=("$f")
-        fi
-    done
-    echo "Upload terminé : ${#SUCCESS_FILES[@]} OK, ${#FAIL_FILES[@]} échoué(s)"
+[[ ${#} -ge 1 ]] || : ${EXCEPTION:?"No file provided. Exiting."}
 
-    if [ ${#FAIL_FILES[@]} -gt 0 ]; then
-        echo "Fichiers échoués :"
-        for f in "${FAIL_FILES[@]}"; do
-            echo "  - $f"
-        done
-    fi
-    get_quota
-    logout
+login
+get_quota
+TOTAL=${#}
+COUNT=0
+SUCCESS_FILES=()
+FAIL_FILES=()
+for f; do # parcours les parametres
+  COUNT=$((COUNT + 1))
+  echo "Transferring file $COUNT/$TOTAL"
+  FILESIZE="$(du -b "$f" | cut -f1)"
+  if [[ ${FILESIZE} -gt ${FREEQUOTA:?variable non définie} ]]; then
+    FILESIZE_MB=$(( FILESIZE / 1024 / 1024 ))
+    echo "File ${f} is too large to upload (${FILESIZE_MB} MB). Skipping."
+    FAIL_FILES+=("${f}")
+    continue
+  fi
+  upload "${f}"
+  if [[ ${?} -eq 0 ]]; then
+    SUCCESS_FILES+=("${f}")
+    FREEQUOTA=$((FREEQUOTA - FILESIZE))
+   else
+     FAIL_FILES+=("${f}")
+   fi
+done
+echo "Upload terminé : ${#SUCCESS_FILES[@]} OK, ${#FAIL_FILES[@]} échoué(s)"
+
+if [[ ${#FAIL_FILES[@]} -gt 0 ]]; then
+  echo "Fichiers échoués :"
+  for f in "${FAIL_FILES[@]}"; do
+    echo "  - $f"
+  done
 fi
-
-
+get_quota
+logout
